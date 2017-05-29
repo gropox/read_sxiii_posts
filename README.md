e1dc6deb64ca91d66e4465d43c1ef6fa044706dd
Georg Sawtschuk <georg.the.moon@googlemail.com>
2017-05-29 08:46:23 +0200

    script

---
+++ README.md
@@ -0,0 +1,22 @@
+Для исполнения потребуется node >= 7 
+
+Как установить
+
+```
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+```
+
+Установить библиотеку стимит
+
+```
+npm install steem
+```
+
+Запустить скрипт
+
+
+```
+node get_sexiii_blog.js 
+```
+
---
+++ get_sexiii_blog.js
@@ -0,0 +1,81 @@
+
+
+var steem = require("steem")
+
+steem.config.set('websocket',"wss://ws.golos.io");
+steem.config.set('address_prefix',"GLS");
+steem.config.set('chain_id','782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12');
+
+
+const USERID = "sxiii";
+
+var LAST_READ_ID;
+
+async function readHistory() {
+    
+    //get max histopry  ID 
+    let heArr = await steem.api.getAccountHistoryAsync(USERID, -1, 0);
+    
+    const LAST_ID = heArr[0][0];
+
+    let start = 2001;
+    let count = 2000;
+    
+    while(start < LAST_ID) {
+        console.log("start = " + start + " count = " + count);
+        let heArr = await steem.api.getAccountHistoryAsync(USERID, start, count);
+        filterAccountHistory(heArr);
+        if(start + count > LAST_ID) {
+            count = LAST_ID - start;
+            start = LAST_ID;
+        } else {
+            start += count;
+        }
+    }
+    
+    console.log("найдено постов " + PERMLINKS.length);
+    
+    process.exit(0);
+}
+
+async function filterAccountHistory(heArr) {
+
+    for(let i = 0; i < heArr.length; i++) {
+        let he = heArr[i];
+        let id = he[0];
+        if(id <= LAST_READ_ID) {
+            continue; //что бы не заморачиваться с математикой индексов и не читать повторно
+        }
+        LAST_READ_ID = id;
+        
+        let tr = he[1];
+        
+        processTransaction(tr);
+    }
+}
+
+var PERMLINKS = [];
+
+async function processTransaction(tr) {
+    /*
+        * tr.block
+        * tr.timestamp
+        * tr.op[0] //имя операции
+        * tr.op[1] //операция
+        */
+
+    let op = tr.op[0];
+    let opBody = tr.op[1];
+    if(op == "comment" && opBody.parent_author == "") {
+   
+        if(PERMLINKS.includes(opBody.permlink)) {
+            //ледактирование поста, игнорируем
+            console.log("редактирование " + opBody.permlink);
+            return;
+        }
+        PERMLINKS.push(opBody.permlink);
+        console.log(tr.block + "\t" + tr.timestamp + "\t" + JSON.stringify(opBody.body));
+    }
+}
+
+readHistory();
